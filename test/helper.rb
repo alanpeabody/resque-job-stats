ba792b8c1cd228fbeeab961dfbde0d27886130e9
Damon Morgan <damon.morgan@gmail.com>
2012-05-22 14:38:35 +0100

    Make test runs self contained
    Start own instance of redis for test runs.

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,30 @@ require 'redgreen'
 require 'resque'
 require 'timecop'
 
-Resque.redis = 'localhost:6379'
+#
+# make sure we can run redis
+#
+if !system("which redis-server")
+  puts '', "** can't find `redis-server` in your path"
+  puts "** add redis-server to your PATH and try again"
+  abort ''
+end
+
+#
+# start our own redis when the tests start,
+# kill it when they end
+#
+dir = File.dirname(__FILE__)
+at_exit do
+  pid = `ps -e -o pid,command | grep [r]edis-test`.split(" ")[0]
+  puts "Killing test redis server [#{pid}]..."
+  `rm -f #{dir}/dump.rdb`
+  Process.kill("KILL", pid.to_i)
+end
+
+puts "Starting redis for testing at localhost:9736..."
+`redis-server #{dir}/redis-test.conf`
+Resque.redis = 'localhost:9736'
 Resque.redis.namespace = 'resque:job_stats'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
